de576fac4417dc73269ae22d12b023dcdedff5a0
azzamaflah <azzamhebat46@gmail.com>
2024-12-29 20:54:45 +0700

    Fungsi untuk memetakan InsertUiEvent menjadi Mahasiswa

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/InsertViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase.model.Mahasiswa
 import com.example.roomdatabase.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
@@ -26,3 +27,27 @@ class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
         }
     }
 }
+
+    data class InsertUiState(
+        val insertUiEvent: InsertUiEvent = InsertUiEvent()
+    )
+
+    data class InsertUiEvent(
+        val nim: String = "",
+        val nama: String = "",
+        val alamat: String = "",
+        val jenisKelamin: String = "",
+        val kelas: String = "",
+        val angkatan: String = ""
+    )
+    fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+        nim = nim,
+        nama = nama,
+        alamat = alamat,
+        jenisKelamin = jenisKelamin,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+
+
+
