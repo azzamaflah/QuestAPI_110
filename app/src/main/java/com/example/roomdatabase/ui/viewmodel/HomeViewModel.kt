b5f4a3303d481c0f4c7c00d06d9a9084b5fe7043
azzamaflah <azzamhebat46@gmail.com>
2024-12-29 21:03:35 +0700

    untuk mengambil data mahasiswa dari repository

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.roomdatabase.ui.viewmodel
 
+import android.net.http.HttpException
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomdatabase.model.Mahasiswa
 import com.example.roomdatabase.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -19,6 +23,19 @@ class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
 
     init {
         getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
     }
 
 }
