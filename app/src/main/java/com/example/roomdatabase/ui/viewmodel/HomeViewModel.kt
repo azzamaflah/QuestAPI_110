5fbb2e7c59ede9fb733cb7b47caf821e3c6a093b
azzamaflah <azzamhebat46@gmail.com>
2024-12-29 21:01:35 +0700

    untuk memonitor perubahan pada layar Home

---
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.roomdatabase.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.model.Mahasiswa
+import com.example.roomdatabase.repository.MahasiswaRepository
+
+sealed class HomeUiState {
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+}
