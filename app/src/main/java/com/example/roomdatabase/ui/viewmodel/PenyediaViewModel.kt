07fdc767fccbece3e8d57c123a8c18eaf112666a
azzamaflah <azzamhebat46@gmail.com>
2024-12-29 22:22:55 +0700

    untuk mendapatkan instance MahasiswaApplications dari CreationExtras

---
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.roomdatabase.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiKontak().container.mahasiswaRepository)
+        }
+        initializer {
+            InsertViewModel(aplikasiKontak().container.mahasiswaRepository)
+        }
+    }
+}
+fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
