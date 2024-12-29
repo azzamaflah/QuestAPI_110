baecfe11dfb3889234748f5e27d5af54f5d1f26d
azzamaflah <azzamhebat46@gmail.com>
2024-12-29 22:29:13 +0700

    untuk mengatur navigasi antara layar Home dan Entry

---
+++ app/src/main/java/com/example/roomdatabase/ui/navigation/PengelolahHalaman.kt
@@ -0,0 +1,44 @@
+package com.example.roomdatabase.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.roomdatabase.ui.view.DestinasiEntry
+import com.example.roomdatabase.ui.view.DestinasiHome
+import com.example.roomdatabase.ui.view.EntryMhsScreen
+import com.example.roomdatabase.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        // Komposisi untuk layar Home
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
